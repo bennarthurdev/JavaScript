99d331f03bb707082541de35d017924d18eb1484
Benn Arthur <87674915+bennarthurdev@users.noreply.github.com>
2022-11-01 12:06:41 -0300

    New uploads : )

--- FUNCOES/return.js
+++ FUNCOES/return.js
@@ -1,3 +1,20 @@
+//return sum between a and b
+function sum(a,b){
+    return a+b;
+}
+//return objects
+function createPerson(name, lastname){
+    return {name, lastname};
+}
+const p1 = createPerson('Benn' , 'Amaral');
+console.log(p1);
+//is the same input of
+const p2 = {
+    name:'Benn',
+    lastname:'Amaral'
+}
+console.log(p2)
+//return somatory
 function createSomatory(somatory){
     
     return function(x){
@@ -10,6 +27,8 @@ const three = createSomatory(3);
 const four = createSomatory(4);
 
 //calling functions
+/*
 console.log(two(2));
 console.log(three(3));
-console.log(four(4));
+console.log(four(4));*/
+//console.log(sum(2,5));
